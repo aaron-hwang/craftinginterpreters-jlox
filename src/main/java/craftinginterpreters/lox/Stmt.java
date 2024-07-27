921d48cae7ec697707337eaddec9c8038171f30e
ducc <aaronhwang22@gmail.com>
2024-07-27 14:33:04 -0400

    Updated/Regenerated Expr and Stmt.java, updated GenerateAST to add support for vars

--- src/main/java/craftinginterpreters/lox/Stmt.java
+++ src/main/java/craftinginterpreters/lox/Stmt.java
@@ -1,9 +1,12 @@
 package craftinginterpreters.lox;
 
+import java.util.List;
+
 abstract class Stmt {
  interface Visitor<R> {
     R visitExpressionStmt(Expression stmt);
     R visitPrintStmt(Print stmt);
+    R visitVarStmt(Var stmt);
     }
  static class Expression extends Stmt {
     Expression(Expr expression) {
@@ -28,6 +31,20 @@ abstract class Stmt {
     }
 
     final Expr expression;
+ }
+ static class Var extends Stmt {
+    Var(Token name, Expr initializer) {
+    this.name = name;
+    this.initializer = initializer;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitVarStmt(this);
+    }
+
+    final Token name;
+    final Expr initializer;
  }
 
     abstract <R> R accept(Visitor<R> visitor);

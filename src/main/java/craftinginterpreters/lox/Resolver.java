98f86303d634b5d4b9dc784e650805534d8b04a3
ducc <aaronhwang22@gmail.com>
2024-08-05 22:38:38 -0400

    Added error handling for super keyword

--- src/main/java/craftinginterpreters/lox/Resolver.java
+++ src/main/java/craftinginterpreters/lox/Resolver.java
@@ -30,6 +30,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     private enum ClassType {
         NONE,
         CLASS,
+        SUBCLASS,
     }
 
     @Override
@@ -53,7 +54,13 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         }
 
         if (stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
+        }
+
+        if (stmt.superclass != null) {
+            beginScope();
+            scopes.peek().put("super", true);
         }
 
         beginScope();
@@ -67,6 +74,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
             resolveFunction(method, declaration);
         }
         endScope();
+
+        if (stmt.superclass != null) endScope();
 
         currentClass = enclosingClass;
         return null;
@@ -225,6 +234,19 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     public Void visitSetExpr(Expr.Set expr) {
         resolve(expr.value);
         resolve(expr.object);
+        return null;
+    }
+
+    @Override
+    public Void visitSuperExpr(Expr.Super expr) {
+        // Check for valid super usage
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Cannot call super outside of a class");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword, "Cannot call super in a class with no superclass");
+        }
+
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
